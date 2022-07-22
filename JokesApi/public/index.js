a7c538b7011a4051653936498823b4a40429d4b2
Raashish Aggarwal <94279692+raashish1601@users.noreply.github.com>
2022-07-22 11:54:00 +0530

    Add files via upload

--- JokesApi/public/index.js
+++ JokesApi/public/index.js
@@ -1,1 +1,23 @@
+async function getJoke() {
+  const data = await fetch(" https://icanhazdadjoke.com/", {
+    headers: { Accept: "application/json" },
+  });
+  const obj = await data.json();
+  console.log(obj.joke);
+  document.getElementById("joke").innerHTML = obj.joke;
+}
 
+async function sendToBackend() {
+  alert("Added to Database");
+  const joke = document.getElementById("joke");
+  const jokeContent = joke.innerText;
+  const response = await fetch("/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ joke: jokeContent.toString() }),
+  });
+  const data = await response.json();
+  console.log(data);
+}
